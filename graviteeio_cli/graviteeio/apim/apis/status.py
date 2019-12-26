e54521069188eba38dd69ebc465af5b8edf1c1bd
Guillaume Gillon <guillaume.gillon@gmail.com>
2019-12-26 14:14:41 +0100

    Refactor status command

--- graviteeio_cli/graviteeio/apim/apis/status.py
+++ graviteeio_cli/graviteeio/apim/apis/status.py
@@ -1,5 +1,10 @@
 import click
+import time
+import jmespath
 from graviteeio_cli.graviteeio.output import OutputFormat, gio, FormatType
+from ....exeptions import GraviteeioError
+
+colors = {"1xx":"white","2xx":"green","3xx":"white","4xx":"yellow","5xx":"red"}
 
 @click.command()
 @click.argument('api_id', required=True)
@@ -7,49 +12,60 @@ from graviteeio_cli.graviteeio.output import OutputFormat, gio, FormatType
               default="table",
               help='Set the format for printing command output resources. The supported formats are: `table`, `json`, `yaml`, `tsv`. Default is: `table`',
               type=click.Choice(FormatType.list_name(), case_sensitive=False))
+@click.option('--query',
+              default="[].{Status: status, Hits: hits, Percent: percent}",
+              help='Execute JMESPath query. eg: filtered on 5xx status `[?status==`5xx`].{Status: status, Hits: hits, Percent: percent}`' )
 @click.pass_obj
-def status(obj, format, api_id):
-    """This command displays API status"""
+def status(obj, format, query, api_id):
+    """
+This command displays API status
+
+\b
+Status Field:
+- `status`: string 
+- `hits`: numerate
+- `percent`: string
+    """
     api_client = obj['api_client']
     status_values = api_client.status(api_id).json()['values']
 
-    status_100_200 = status_values['100.0-200.0']
-    status_200_300 = status_values['200.0-300.0']
-    status_300_400 = status_values['300.0-400.0']
-    status_400_500 = status_values['400.0-500.0']
-    status_500_600 = status_values['500.0-600.0']
+    to_return = []
+    total = 0
+    for key,value in status_values.items():
+        status_str = "{}xx".format(key[0:1])
+        color = colors[status_str]
+        to_return.append(
+            {
+                "status": status_str,
+                "hits": value,
+                "percent": value * 100,
+            }
+        )
+        total += value
+    # style()
+    if total > 0:
+        for status in to_return:
+            status["percent"] = round(status["percent"] / total, 2)
+ 
+    # start_time = time.time()
+    try:
+        status_filtered = jmespath.search(query, to_return)
+    
+        if len(to_return) > 0:
+            header = status_filtered[0].keys()
+            
+        outputFormat = OutputFormat.value_of(format)
+        gio.echo(status_filtered, outputFormat, header)
 
-    total = status_100_200 + status_200_300 + status_300_400 + status_400_500 + status_500_600
-    if total == 0:
-        total = 1
+    except Exception as err:
+        raise GraviteeioError(err.msg)
 
-    to_return =[
-        {
-            "status": "1xx",
-            "hits": status_100_200,
-            "percent": round((status_100_200 * 100) / total, 2)
-        },
-        {
-            "status": "2xx",
-            "hits": status_200_300,
-            "percent": round((status_200_300 * 100) / total, 2)
-        },
-        {
-            "status": "3xx",
-            "hits": status_300_400,
-            "percent": round((status_300_400 * 100) / total, 2)
-        },
-        {
-            "status": "4xx",
-            "hits": status_400_500,
-            "percent": round((status_400_500 * 100) / total, 2)
-        },
-        {
-            "status": "5xx",
-            "hits": status_500_600,
-            "percent": round((status_500_600 * 100) / total, 2)
-        }
-    ]
+    # outputFormat = OutputFormat.value_of(format)
+    # gio.echo(to_return, outputFormat, ["","",""])
+    # print("Temps d execution : %s secondes ---" % (time.time() - start_time))
 
-    outputFormat = OutputFormat.value_of(format)
-    gio.echo(to_return, outputFormat, ["Status","Hits", "Percent"])
+def style(value, format, color):
+    if format is 'table':
+        return click.style("{}".format(value), fg=color)
+    else:
+        return value
