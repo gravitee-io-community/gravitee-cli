9a25c892cf6def8d1f0c77ee91eb6d1c35bae254
Guillaume Gillon <guillaume.gillon@gmail.com>
2020-05-07 21:52:13 +0200

    Fix: apis list

--- graviteeio_cli/graviteeio/apim/apis/apis.py
+++ graviteeio_cli/graviteeio/apim/apis/apis.py
@@ -4,7 +4,7 @@ import sys
 import click
 from graviteeio_cli.graviteeio.modules import GraviteeioModule
 
-from graviteeio_cli.graviteeio.client.gio import gio
+from graviteeio_cli.graviteeio.client.gio import gio, APIM_http_client_type
 from .plugins import COMMANDS
 import pkgutil
 
@@ -27,14 +27,17 @@ class PluginCommand(click.MultiCommand):
         try:
             if sys.version_info[0] == 2:
                 name = name.encode('ascii', 'replace')
+            
+            methode_name = name
             if name in add_command:
-                if name is 'list': name = "ls"
-                mod = __import__('graviteeio_cli.graviteeio.apim.apis.' + name, None, None, [name])
+                if name == 'list': methode_name = "ls"
+                mod = __import__('graviteeio_cli.graviteeio.apim.apis.' + methode_name, None, None, [methode_name])
             else:
-                mod = __import__('graviteeio_cli.graviteeio.apim.apis.plugins.cmd_' + name, None, None, [name])
+                mod = __import__('graviteeio_cli.graviteeio.apim.apis.plugins.cmd_' + methode_name, None, None, [methode_name])
         except ImportError:
             return
-        return getattr(mod, name)
+
+        return getattr(mod, methode_name)
 
 
 @click.command(cls=PluginCommand)
@@ -43,4 +46,4 @@ def apis(ctx):
     """
     apis commands
     """
-    ctx.obj['api_client'] = gio(config=ctx.obj['config'].getGraviteeioConfig(GraviteeioModule.APIM)).apim_api
+    ctx.obj['api_client'] = gio.HttpClient_APIM(config=ctx.obj['config'].getGraviteeioConfig(GraviteeioModule.APIM), http_client_type = APIM_http_client_type.API)
