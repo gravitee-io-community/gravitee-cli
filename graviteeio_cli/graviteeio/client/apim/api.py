91da88ddb2d0a3005c5fe314f18c3c2e4808cdd9
Guillaume Gillon <guillaume.gillon@gmail.com>
2020-06-30 09:49:52 +0200

    feat: add log and exclude metadata for export in client module

--- graviteeio_cli/graviteeio/client/apim/api.py
+++ graviteeio_cli/graviteeio/client/apim/api.py
@@ -45,7 +45,7 @@ class ApiClient:
     
     def get_export(self, id, response_filter = None):
         params = {
-            "exclude": "groups,members,pages",
+            "exclude": "groups,members,pages,metadata",
             "version": "default"
         }
 
@@ -110,3 +110,32 @@ class ApiClient:
 
     def pages_fetch(self, id):
         return self.httpClient.post("{}/pages/_fetch".format(id))
+
+    def logs(self, id, line, from_time = None, to_time = None, time_frame_seconds = None, order = False):
+        if not from_time and not to_time and not time_frame_seconds:
+            return None
+
+        from_timestamp = from_time
+        to_timestamp = to_time
+
+        if time_frame_seconds:
+            now = datetime.now()
+            new_date = now - timedelta(seconds=time_frame_seconds)
+
+            from_timestamp = int(new_date.timestamp() * 1000)
+            to_timestamp = int(now.timestamp() * 1000)
+
+        logs_total_metadata = self.httpClient.get("{}/logs?page=1&size={}&from={}&to={}&field=@timestamp&order={}".format(id, line, from_timestamp, to_timestamp, order)).json()
+
+        logs = None
+        total = None
+        if "logs" in logs_total_metadata and "total" in logs_total_metadata and "metadata" in logs_total_metadata:
+            logs = logs_total_metadata["logs"]
+            total = logs_total_metadata["total"]
+            metadata = logs_total_metadata["metadata"]
+
+            for log in logs:
+                log["application_name"] = metadata[log["application"]]["name"]
+                log["plan_name"] = metadata[log["plan"]]["name"]
+        
+        return (logs_total_metadata, from_timestamp, to_timestamp)
