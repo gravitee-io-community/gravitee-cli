8164c776aed3ebc64957f7e8e67f1ab8ce632a2b
Guillaume Gillon <guillaume.gillon@gmail.com>
2020-07-17 10:01:33 +0200

    new script build and deploy

---
+++ bin/build-deploy-pypi.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BASH_PATH="`dirname \"$0\"`"
+
+if [ -z "$BASH_PATH" ] ; then
+  exit 1  # fail
+fi
+
+cd $BASH_PATH
+cd ..
+
+rm -r dist/
+sudo python3 -m pip install --upgrade setuptools wheel
+python3 setup.py bdist_wheel
+python3 -m twine upload dist/*
